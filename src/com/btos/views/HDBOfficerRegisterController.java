c32666ca2cc17a88f67fc9cb3bb857116b11015a
zachzz <62929716+zhouyingli@users.noreply.github.com>
2025-04-12 17:37:05 +0800

    Update HDBOfficerRegisterController.java

--- src/com/btos/views/HDBOfficerRegisterController.java
+++ src/com/btos/views/HDBOfficerRegisterController.java
@@ -1,13 +1,33 @@
 
-public class HDBOfficerRegisterController {
-    private List<HDBOfficer> officers;
+import java.util.List;
 
-    public HDBOfficerRegisterController() {
-        this.officers = new ArrayList<>();
+public class HDBOfficerRegisterController {
+    private final List<BTOProject> projectList;
+
+    public HDBOfficerRegisterController(List<BTOProject> projectList) {
+        this.projectList = projectList;
     }
 
-    public void registerOfficer(HDBOfficer officer) {
-        officers.add(officer);
-        System.out.println("Officer registered: " + officer.getName());
+    public void registerForProject(HDBOfficer officer, BTOProject project) {
+        if (!officer.isEligibleForProject(project)) {
+            System.out.println("You are not eligible to register for this project.");
+            return;
+        }
+
+        if (project.getAvailableOfficerSlots() > 0) {
+            project.registerOfficer(officer);
+            System.out.println("Registration request submitted.");
+        } else {
+            System.out.println("No available slots for officers.");
+        }
+    }
+    
+    public void validateEligibility(HDBOfficer officer, BTOProject projectList) {
+    	
+    }
+
+    public void checkRegistrationStatus(HDBOfficer officer) {
+        System.out.println("Current registered project: " + officer.getHandlingProject());
     }
 }
+
